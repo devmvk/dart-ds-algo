60c429647a65d1902f040cd7c195f9800043f389
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-06-04 22:55:12 +0530

    adds problem statement

--- string/reverse_string_words.dart
+++ string/reverse_string_words.dart
@@ -1,3 +1,11 @@
+/*
+Reverse words in a given string
+https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1#
+
+Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+*/
+
 String reverseStringInWords(String value) {
   return value.split(".").reversed.toList().join(".");
 }
