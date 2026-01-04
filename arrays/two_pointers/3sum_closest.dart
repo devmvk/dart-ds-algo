6707e1a8f41dd3f33b74cbcde570484113dbf2f2
Varun Madupu <developer.varunkumar@gmail.com>
2026-01-04 15:36:42 +0530

    3sum closest

---
+++ arrays/two_pointers/3sum_closest.dart
@@ -0,0 +1,68 @@
+/* 
+1. 3Sum Closest	
+• 3 <= nums.length <= 1000
+• -1000 <= nums[i], target <= 1000
+• There is exactly one unique solution.
+Find the sum of three integers such that the sum is closest to target. Return the sum itself.
+
+*/
+
+
+int threeSumClosestAI(List<int> nums, int target) {
+  nums.sort();
+  int closestSum = nums[0] + nums[1] + nums[2];
+
+  for (int i = 0; i < nums.length - 2; i++) {
+    int left = i + 1;
+    int right = nums.length - 1;
+
+    while (left < right) {
+      int currentSum = nums[i] + nums[left] + nums[right];
+
+      if ((currentSum - target).abs() < (closestSum - target).abs()) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum < target) {
+        left++;
+      } else if (currentSum > target) {
+        right--;
+      } else {
+        return currentSum; // Exact match found
+      }
+    }
+  }
+
+  return closestSum;
+
+}
+
+int threeSumClosest(List<int> nums, int target){
+	
+	nums.sort();
+	int minDifferance = 1000;
+	int result = 0;
+
+	for(int i=0; i< nums.length-2; i++){
+		int left = i+1;
+  	int right = nums.length - 1;
+
+		while(left < right){
+
+			int sum = nums[i] + nums[left] + nums[right];
+			int differance = (target - sum).abs();
+			if(differance < minDifferance){
+		    	result = sum;
+		    	minDifferance = differance;
+		    }
+
+		    if(sum < target) left++;
+		    else if(sum > target) right--;
+		    else {
+		       return sum;
+		    }
+		}
+	}
+
+	return result;
+}
