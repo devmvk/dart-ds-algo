1a8cce5bff01d7e7514579e1f17bf0d23409a1f8
Varun Madupu <developer.varunkumar@gmail.com>
2026-01-04 15:53:11 +0530

    3 sum smaller

---
+++ arrays/two_pointers/3sum_smaller.dart
@@ -0,0 +1,43 @@
+int threeSumSmallerAI(List<int> nums, int target) {
+  nums.sort();
+  int count = 0;
+
+  for (int i = 0; i < nums.length - 2; i++) {
+    int left = i + 1;
+    int right = nums.length - 1;
+
+    while (left < right) {
+      int currentSum = nums[i] + nums[left] + nums[right];
+
+      if (currentSum < target) {
+        count += (right - left);
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return count;
+}
+
+int threeSumSmaller(List<int> nums, int target) {
+  nums.sort();
+  int count = 0;
+
+  for (int i = 0; i < nums.length - 2; i++) {
+    int left = i + 1;
+    int right = nums.length - 1;
+
+    while (left < right) {
+      int sum = nums[i] + nums[left] + nums[right];
+      if (sum < target) {
+        left++;
+        count++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return count;
+}
